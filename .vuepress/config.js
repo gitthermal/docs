e3ab606c31018520ecaf081f6a22ea32b9909ddf
Yashu Mittal <mittalyashu77@gmail.com>
2019-04-06 11:45:08 +0530

    Add other meta  tags (facebook & twitter)

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,19 @@ module.exports = {
   description: 'One stop. Git repository',
   dest: "dist",
   head: [
-    ['link', { rel: 'icon', href: '/images/favicon.png' }]
+    ['link', { rel: 'icon', href: '/images/favicon.png' }],
+    ['meta', { name: 'keywords', content: 'thermal, thermal app, git thermal, thermal gui, thermal git, git gui' }],
+    ['meta', { name: 'author', content: 'CodeCarrot and Yashu Mittal' }],
+    ['meta', { itemprop: 'image', content: '/images/meta_image.png' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { itemprop: 'og:image', content: '/images/meta_image.png' }],
+    ['meta', { property: 'og:site_name', content: 'Thermal' }],
+    ['meta', { property: 'og:description', content: 'One stop. Git repository' }],
+    ['meta', { property: 'twitter:description', content: 'One stop. Git repository' }],
+    ['meta', { property: 'twitter:image', content: '/images/meta_image.png' }],
+    ['meta', { property: 'twitter:url', content: 'https://thermal.netlify.com/' }],
+    ['meta', { property: 'twitter:site', content: '@gitthermal' }],
+    ['meta', { name: 'robots', content: 'index, follow' }]
   ],
   ga: "UA-131193769-3",
   themeConfig: {

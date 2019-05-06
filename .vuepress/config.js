e60406d969dd3e28ac9def35ec39fb2bb956dcb0
Yashu Mittal <mittalyashu77@gmail.com>
2019-05-06 11:10:57 +0530

    Remove link to support and changelog

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -28,13 +28,6 @@ module.exports = {
 					{ text: 'Windows', link: '/win/guide/' }
 				]
 			},
-			{
-				text: 'Misc',
-				items: [
-					{ text: 'Support', link: 'https://discord.gg/KT3nAR5' },
-					{ text: 'Changelog', link: 'https://www.notion.so/gitthermal/Release-notes-7d77751a39484413b7ca8564b3f390a9' }
-				]
-			},
 			{ text: 'Patron', link: 'https://www.patreon.com/join/mittalyashu?' }
 		],
 		sidebar: {
